e3d86dfa14c5141d2a354dbe7595e2ccef13f020
preetcoder12 <preetgusainn23@gmail.com>
2025-02-15 21:07:53 +0530

    contest Array ques

---
+++ ARRAY/Pointers/4_Sum.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int> &nums, int target)
+{
+    vector<vector<int>> result;
+
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+
+    for (int i = 0; i < n - 3; i++)
+    {
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        for (int j = i + 1; j < n - 2; j++)
+        {
+            if (j > i + 1 && nums[j] == nums[j - 1])
+            {
+                continue;
+            }
+            int left = j + 1, right = n - 1;
+            while (left < right)
+            {
+                long long sum = (long long)nums[i] + nums[j] + nums[left] + nums[right];
+                if (sum == target)
+                {
+                    result.push_back({nums[i], nums[j], nums[left], nums[right]});
+
+                    while (left < right && nums[left] == nums[left + 1])
+                    {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1])
+                    {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if (sum < target)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+int main()
+{
+    vector<int> nums = {2, 2, 2, 2, 1, 3};
+    int target = 8;
+    vector<vector<int>> quadruplets = fourSum(nums, target);
+    cout << quadruplets.size() << endl; // Output: 2
+    return 0;
+}
