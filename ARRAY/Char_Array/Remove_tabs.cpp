705a0bcf6e575465614fdb3c56db4a4de8527254
preetcoder12 <preetgusainn23@gmail.com>
2025-03-13 00:06:52 +0530

    second largest without sort

--- ARRAY/Char_Array/Remove_tabs.cpp
+++ ARRAY/Char_Array/Remove_tabs.cpp
@@ -5,7 +5,9 @@ int main()
 {
     string str = "my name is                            preet";
     auto new_end = unique(str.begin(), str.end(), [](char a, char b)
-                          { return a == ' ' && b == ' '; });
+    { 
+        return a == ' ' && b == ' '; 
+    });
     str.erase(new_end, str.end());
     cout << str << endl;
     return 0;
